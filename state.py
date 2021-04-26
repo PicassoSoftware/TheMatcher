27df540b2907deeb8301cc0c00818eaf8e215c07
Sırat Semih Çöp <68740910+s1rat-dev@users.noreply.github.com>
2021-04-26 16:57:19 +0300

    v.2
    Nfa ve Dfa için ayrı ayrı listeler ve kütüphaneler oluşturuldu.

--- state.py
+++ state.py
@@ -7,24 +7,44 @@ class State():
 
 class StateControl():
     def __init__(self):
-        self.allStates = []
+        self.allStatesForNfa = []
+        self.allStatesForDfa = []
 
     def addState(self, obj: State):
-        stateDict = self.ready2Add(obj)
-        self.allStates.append(stateDict)
+        stateDictForNfa, stateDictForDfa = self.ready2Add(obj)
+        self.allStatesForDfa.append(stateDictForDfa)
+        self.allStatesForNfa.append(stateDictForNfa)
 
 
     def ready2Add(self, obj: State):
-        stateDict = {}
+
+        #                       FOR NFA                         #
+        #########################################################
+        stateDictForNfa = {}
+        stateDictForNfa.update(
+            {
+                'state': obj.stateValue,
+                'next': obj.next,
+                'char': obj.values
+            }
+        )
+        ##########################################################
+
+
+        #                        FOR DFA                         #
+        ##########################################################
+        stateDictForDfa = {}
         stateProperties = dict(zip(obj.values, obj.next))
         stateMood = obj.stateValue
-        stateDict.setdefault('state', stateMood)
-        stateDict.setdefault('properties', stateProperties)
+        stateDictForDfa.setdefault('state', stateMood)
+        stateDictForDfa.setdefault('properties', stateProperties)
         '''
             e.g.:
                 {'state': 0, 'properties': {'a': 0, 'b': 2}}
                 {'state': 1, 'properties': {'b': 2}}
                 {'state': 2, 'properties': {'c': 3}}
-                {'state': 3, 'properties': {'d': 0}}                       
+                {'state': 3, 'properties': {'d': 0}}
         '''
-        return stateDict
+        ###########################################################
+
+        return stateDictForNfa,stateDictForDfa
