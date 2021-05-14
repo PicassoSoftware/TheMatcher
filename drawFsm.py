18988b69dc22dcfb6759b516fa14d10495cc7dd1
furkansefademirci <52625217+furkansefademirci@users.noreply.github.com>
2021-05-14 17:21:30 +0300

    Add files via upload

--- drawFsm.py
+++ drawFsm.py
@@ -1,56 +1,51 @@
-from reg2nfa import formal_nfa,transition
 from graphviz import Digraph
 import time
-class FsmDrawer:
-#statemanager içinde kullanmak için regex parametresi eklenmeli. 
-# self.nfa = regex açılmalı
+from showFsm import Window
 
-    def __init__(self): 
-        # self.nfa = regex
-        self.nfa = formal_nfa("(a|b)*aba")
-        self.createRef()
-        
-
-    def createRef(self):
-        self.f = Digraph("FSM","FSMtext.txt")
-        self.f.attr("node" , shape = "doublecircle")
-        self.f.attr(rankdir = "LR", size = "12")
-        self.f.node(str(self.nfa.accept))
+class FsmDrawer():
+    def __init__(self,formalDefinition): 
+        self.formalDefinition = formalDefinition
+        self.createReference()
+       
+    
+    def createReference(self):
+        self.fsm = Digraph("FSM","FSMtext.txt",format="svg")
+        self.fsm.attr("node" , shape = "doublecircle") #fontsize can be resize
+        self.fsm.attr(rankdir = "LR")
+        self.fsm.node(str(self.formalDefinition.accept))
+        self.img = Window()
+        print(str(self.formalDefinition.accept))
 
     def drawFsm(self,fsmStart,fsmNext,fsmLabel):
-        self.f.attr("node" ,shape = "circle")
-        self.f.edge(fsmStart,fsmNext,label = fsmLabel)
+        self.fsm.attr("node" ,shape = "circle")
+        self.fsm.edge(fsmStart,fsmNext,label = fsmLabel)
 
     def drawFsmColored(self,fsmStart,fsmNext,fsmLabel):
-        self.f.attr("node",shape = "circle")
-        self.f.edge(fsmStart,fsmNext,fontcolor = "blue",label = fsmLabel)
+        self.fsm.attr("node",shape = "circle")
+        self.fsm.edge(fsmStart,fsmNext,fontcolor = "red",label = fsmLabel)
   
 
-    def click(self,char,first):
-        for a in self.nfa.transitions:
+    def click(self,char,first):        
+        for a in self.formalDefinition.transitions:
             if((str(a.regex) == char) & (str(a.start) == first) ):
                 self.drawFsmColored(str(a.start),str(a.end),str(a.regex))
             else:
                 self.drawFsm(str(a.start),str(a.end),str(a.regex))  
-        self.f.view()
-        self.clearRef()
-    
-    def clearRef(self):
-        time.sleep(1)
-        self.f.clear()
-        self.createRef()
 
-# Aşağıdaki liste içinde olanlar dinamik olarak gelmeli
-listText = ["a","b","a","a","b","a"]
-listFirst = ["1","2","3","1","2","3"]
-z = FsmDrawer()
-for a in range(6):
-    char = listText[a]
-    first = listFirst[a]
-    z.click(char, first)
-      
-      
+        self.fsm.render(view=False)
+
+
         
+        self.clearReference()
+        
+    def clearReference(self):
+        time.sleep(2)
+        self.fsm.clear()
+        self.createReference()
+        
+
+      
+
 
 
 
