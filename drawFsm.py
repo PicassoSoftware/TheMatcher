fa2fe9d6fca6c57f7338f4ff400c188569de1a82
Enyora <77548267+Enyora@users.noreply.github.com>
2021-05-22 15:08:02 +0300

    Update drawFsm.py

--- drawFsm.py
+++ drawFsm.py
@@ -12,25 +12,24 @@ class FsmDrawer():
     
     def createReference(self):
         self.fsm = Digraph("FSM",format="svg",filename="fsm.txt")
-        self.fsm.attr("node" , shape = "doublecircle",color="black",fontsize="10") #fontsize can be resize 
+        self.fsm.attr("node" , shape = "doublecircle",color="#fdfaf6",fontsize="10") #fontsize can be resize
         self.fsm.attr(rankdir = "LR",bgcolor="transparent",size="9,9!")
 
         try:
             for item in self.formalDefinition.accept:
-                self.fsm.node(str(item))
+                self.fsm.node(str(item), fontcolor = "#fdfaf6")
         except:
             self.fsm.node(str(self.formalDefinition.accept))
         self.img = Window(self.label)
         print(str(self.formalDefinition.accept))
 
-    def drawFsm(self,fsmStart,fsmNext,fsmLabel):
-        self.fsm.attr("node" ,shape = "circle",color="black",fontcolor = "black")
-        self.fsm.edge(fsmStart,fsmNext,label = fsmLabel,color="blue",fontcolor = "blue")
+    def drawFsm(self, fsmStart, fsmNext, fsmLabel):
+        self.fsm.attr("node", shape="circle", color="#fdfaf6", fontcolor="#fdfaf6")
+        self.fsm.edge(fsmStart, fsmNext, label=fsmLabel, color="#fdfaf6", fontcolor="#fdfaf6")
 
-
-    def drawFsmColored(self,fsmStart,fsmNext,fsmLabel):
-        self.fsm.attr("node",shape = "circle",color="black",fontcolor = "black")
-        self.fsm.edge(fsmStart,fsmNext,fontcolor = "red",label = fsmLabel,color="red")
+    def drawFsmColored(self, fsmStart, fsmNext, fsmLabel):
+        self.fsm.attr("node", shape="circle", color="#fdfaf6", fontcolor="#fdfaf6")
+        self.fsm.edge(fsmStart, fsmNext, fontcolor="#fb743e", label=fsmLabel, color="#fb743e")
   
 
     def click(self,char,first):       
@@ -40,8 +39,8 @@ class FsmDrawer():
             else:
                 self.drawFsm(str(a.start),str(a.end),str(a.regex))  
 
-        self.fsm.attr("node",shape="plaintext")      
-        self.fsm.edge("",str(self.formalDefinition.start))
+        self.fsm.attr("node",shape="plaintext", color = "#fdfaf6")
+        self.fsm.edge("",str(self.formalDefinition.start),color = "#fdfaf6")
         
         self.fsm.render(view=False)
   
@@ -52,15 +51,3 @@ class FsmDrawer():
         self.fsm.clear()
         self.createReference()
         
-
-      
-
-
-
-
-
-
-
-
-    
-
